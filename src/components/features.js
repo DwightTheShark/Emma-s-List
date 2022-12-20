7f59854f3bdf41c44d1830ff83da3d88eaa0f321
Nicole Schnurr <leannexnicole@gmail.com>
2022-12-20 08:27:54 -0800

    more features work

--- src/components/features.js
+++ src/components/features.js
@@ -1,14 +1,14 @@
-function features() {
-    // const [array, setArray] = useState([1, 2, 3, 4]);
-    const elementToCheck = smalldogs;
+// function features() {
+//     // const [array, setArray] = useState([1, 2, 3, 4]);
+//     const elementToCheck = smalldogs;
   
-    return (
-      <div>
-        {features.map(item => (
-          <div key={features} className={features === elementToCheck ? '' : 'hide'}>
-            {element}
-          </div>
-        ))}
-      </div>
-    );
-  }
+//     return (
+//       <div>
+//         {features.map(item => (
+//           <div key={features} className={features === elementToCheck ? '' : 'hide'}>
+//             {element}
+//           </div>
+//         ))}
+//       </div>
+//     );
+//   }
